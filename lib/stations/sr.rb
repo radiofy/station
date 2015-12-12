d83774fc17ba98b80eac3d91c89d6b445aa1e800
Linus Oleander <linus@oleander.nu>
2015-12-12 05:39:41 +0100

    Set up sr

--- lib/stations/sr.rb
+++ lib/stations/sr.rb
@@ -1,19 +1,17 @@
-# require_relative "../classes/xml"
+module Station
+  file = File.join(File.dirname(__FILE__), "../lists/sr.xml")
+  Nokogiri::XML(File.read(file)).css("channel").each do |channel|
+    Class.new(Format::XML) do
+      config do
+        id channel.attr("name").parameterize
+        url "http://api.sr.se/api/v2/playlists/rightnow?channelid=#{channel.attr("id")}"
+      end
 
-# module Station
-#   class Sr < Format::XML
-#     def process
-#       artist = data.at_css("artist").try(:text)
-#       song = data.at_css("title").try(:text)
-
-#       if artist
-#         artist = artist.split("&").first
-#       end
-
-#       {
-#         artist: artist,
-#         song: song
-#       }
-#     end
-#   end
-# end
+      def process
+        artist = data.at_css("artist").try(:text)
+        song = data.at_css("title").try(:text)
+        { artist: artist, song: song }
+      end
+    end
+  end
+end
