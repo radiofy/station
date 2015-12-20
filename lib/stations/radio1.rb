235fee103cdcedfea40b86c8b8778ad49e19b9ec
Linus Oleander <linus@oleander.nu>
2015-12-20 05:29:50 +0100

    Remove radio1

--- lib/stations/radio1.rb
+++
@@ -1,8 +0,0 @@
-module Station
-  class Radio1 < Format::MTG
-    config do
-      id "radio1"
-      url "http://streamcontrol.mtgradio.se/api/songs/current/1?callback=callback"
-    end
-  end
-end
