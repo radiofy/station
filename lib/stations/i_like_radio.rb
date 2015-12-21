b69a96dcc08ebc232a40bae2d70383ae3fd7c62c
Linus Oleander <linus@oleander.nu>
2015-12-21 23:44:48 +0100

    Add ilikeradio

--- lib/stations/i_like_radio.rb
+++ lib/stations/i_like_radio.rb
@@ -1,14 +1,22 @@
-# JSON.parse(File.read(Rails.root.join("ilikeradio.json"))).each do |s|
-#   station({
-#     station: s.fetch("name").parameterize,
-#     url: "http://unison.mtgradio.se/api/v1/channel?with=currentsong&type[]=live&type[]=playlist",
-#     worker: "StationJob::JSON",
-#     args: { id: s.fetch("id") }
-#   }) do
-#     data   = c.select{ |ss| ss.fetch("id") == args.fetch(:id) }.first
-#     track  = data && data.fetch("currentsong").fetch("song")
-#     artist = track && track.fetch("artist_name")
-#     song   = track && track.fetch("title")
-#     { artist: artist, song: song }
-#   end
-# end
+require "pp"
+
+module Station
+  data = File.read(File.join(File.dirname(__FILE__), "../lists/ilikeradio.json"))
+  JSON.parse(data).each do |station|
+    Class.new(Format::JSON) do
+      config do
+        id station.fetch("name").parameterize
+        url "http://unison.mtgradio.se/api/v1/channel?with=currentsong&type[]=live&type[]=playlist"
+        args [station.fetch("id")]
+      end
+
+      def process(id)
+        raw   = data.select{ |ss| ss.fetch("id") == id }.first
+        track  = raw && raw.fetch("currentsong").fetch("song")
+        artist = track && track.fetch("artist_name")
+        song   = track && track.fetch("title")
+        { artist: artist, song: song }
+      end
+    end
+  end
+end
--- lib/stations/lugna_favoriter.rb
+++
@@ -1,8 +0,0 @@
-module Station
-  class LugnaFavoriter < Format::MTG
-    config do
-      id "lugna-favoriter"
-      url "http://streamcontrol.mtgradio.se/api/songs/current/4?callback=callback"
-    end
-  end
-end
