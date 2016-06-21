ccc424c17addf20f75193fc134fe20dc24749af1
Linus Oleander <linus@oleander.nu>
2016-06-21 20:35:30 +0200

    Fix radiosiljan

--- lib/stations/radiosiljan.rb
+++ lib/stations/radiosiljan.rb
@@ -2,13 +2,12 @@ module Station
   class Radiosiljan < Format::HTML
     config do
       id "radiosiljan"
-      url "http://www.radiosiljan.com/current.html"
+      url "http://radiosiljan.com/sp_update/current.html"
     end
 
     def process
       _, artist, song = data.at_css("body").to_s.match(/<\/b>(.+?) - (.+?)<br>/m).to_a
-      { :artist => (artist), :song => (song) }
-
+      { artist: (artist), song: song }
     end
   end
 end
