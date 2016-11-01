42d8a45edeeac83311c4816ad7eb3ef0bb331f46
Alf Otto Fagermo <fagermo@gmail.com>
2016-11-01 01:57:11 +0100

    Update radio102.rb
    www gives error, while non-www works.

--- lib/stations/radio102.rb
+++ lib/stations/radio102.rb
@@ -2,7 +2,7 @@ module Station
   class Radio102 < Format::JSON
     config do
       id "radio102"
-      url "http://www.radio102.no/ajax.lastplayed.php"
+      url "http://radio102.no/ajax.lastplayed.php"
     end
 
     def process
