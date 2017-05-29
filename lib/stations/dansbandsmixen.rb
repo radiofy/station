87d46d422b4cca5fd390b6876e7c6d6346032afe
Linus Oleander <linus@oleander.nu>
2017-05-29 03:48:57 +0200

    Clean up

--- lib/stations/dansbandsmixen.rb
+++ lib/stations/dansbandsmixen.rb
@@ -1,9 +1,9 @@
-module Station
-  class Dansbandsmixen < Format::MTG
-    config do
-      id "dansbandsmixen"
-      url "http://www.musikmixen.se/stationer/index.php?c=Dansbandsmixen"
-    end
-  end
-end
-
+# module Station
+#   class Dansbandsmixen < Format::MTG
+#     config do
+#       id "dansbandsmixen"
+#       url "http://www.musikmixen.se/stationer/index.php?c=Dansbandsmixen"
+#     end
+#   end
+# end
+# 
